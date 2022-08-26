c0e1c9ed0f4e339cba05b35b7638ebaa90c53f66
Victor Opara <65514728+viktorzee@users.noreply.github.com>
2022-08-26 02:59:11 +0100

    Update 1-calcuator

--- 0x02-python-import_modules/1-calcuator.py
+++ 0x02-python-import_modules/1-calcuator.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-import calcuator_1
-a = 10
-b = 5
-calcuator_1.sub
+
+
+if __name__ == '__main__':
+  
+     from calcuator_1 import add, sub, mul, div
+    
+     a = 10
+     b = 5
+     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+     print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+     print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+     print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
