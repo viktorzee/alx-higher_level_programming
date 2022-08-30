6fcec967a5420cbe94104fdf706084eabed2e863
Victor <opara.victor@gmail.com>
2022-08-29 20:58:20 -0700

    Task three four completed

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+def print reversed_list_integer_list(my_list=[]):
+     if(my_list):
+          my_list.reverse()
+          for list in range(len(my_list):
+               print("{:d}".format(my_list[list]))
