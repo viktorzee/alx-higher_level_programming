f4863b9ca502fb3c2b3be5aa9fb8afdbc0162d44
Victor Opara <65514728+viktorzee@users.noreply.github.com>
2022-08-30 04:43:47 +0100

    Update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,4 @@ def replace_in_list(my_list, idx, element):
      if(idx < 0 or idx > len(my_list) - 1):
           return my_list
      else:
-          return mylist.insert(idx, element)
+          return my_list.insert(idx, element)
