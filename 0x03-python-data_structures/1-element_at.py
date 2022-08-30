6dbba5e9a0656f65e438be8e5564d90a29cd135d
Victor Opara <65514728+viktorzee@users.noreply.github.com>
2022-08-30 04:16:10 +0100

    Update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(my_list, idx):
      if idx < 0: 
           return "None"
-     elif idx > len(my_list):
+     elif idx > len(my_list) - 1:
           return "None"
      else: 
           print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
