e39cf78b625ccd19c558d327d422d357943a2e1c
Victor <opara.victor@gmail.com>
2022-08-31 21:42:15 -0700

    Minor fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
     uniq_list = set(my_list)
     num = 0
-    for val in uniq_list: 
-        num += val
+
+    for i in uniq_list:
+        num += i
 
     return (num)
